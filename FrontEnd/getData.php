a729899671f607ba02e1197fcfd3e9b9bc852b92
pwbf <hunter@pwbf.pw>
2021-04-12 15:57:19 +0800

    Update FrontEnd

--- FrontEnd/getData.php
+++ FrontEnd/getData.php
@@ -4,6 +4,8 @@ const	DB_USER = "astarc";
 const	DB_PASS = "#astarc377";
 const	DB_NAME = "astarc_iot";
 
+const	TP0L_MOD = 50;
+
 if($_GET['r'] == 'TD'){
 	$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 	if ($conn->connect_error) {
@@ -16,7 +18,7 @@ if($_GET['r'] == 'TD'){
 
 	$HL = str_split(($result->fetch_assoc())['VDATA'], 2);
 
-	for($index = 1; $index < 1023; $index++){
+	for($index = 0; $index < 1024; $index++){
 		array_push($TimeDomain, 
 			covint(16,
 				hexdec(
@@ -45,7 +47,7 @@ if($_GET['r'] == 'FD'){
 
 	$HL = str_split(($result->fetch_assoc())['VDATA'], 2);
 
-	for($index = 0; $index < 511; $index++){
+	for($index = 0; $index < 512; $index++){
 		array_push($FreqDomain, 
 			// covint(32,
 				hexdec(
@@ -61,6 +63,56 @@ if($_GET['r'] == 'FD'){
 
 	// echo '<pre>'.print_r($FreqDomain, true).'</pre>';
 	echo json_encode($FreqDomain);
+	$conn->close();
+}
+
+if($_GET['r'] == 'VIBR'){
+	$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+	$sql = 'SELECT VDATA FROM vibrationTD WHERE STATUS = 1 ORDER BY TIMEMARK DESC LIMIT 1';
+	$result = $conn->query($sql);
+	$TimeDomain = array();
+	$HLVIBR = str_split(($result->fetch_assoc())['VDATA'], 2);
+	
+	$sql = 'SELECT VDATA FROM vibrationFD WHERE STATUS = 1 ORDER BY TIMEMARK DESC LIMIT 1';
+	$result = $conn->query($sql);
+	$FreqDomain = array();
+	$rtnData = array();
+	$HLFREQ = str_split(($result->fetch_assoc())['VDATA'], 2);
+	
+	
+	for($index = 0; $index < 512; $index++){
+		array_push($FreqDomain, 
+			// covint(32,
+				hexdec(
+					'0x'. $HLFREQ[( 4 * $index + 3 )]
+						. $HLFREQ[( 4 * $index + 2 )] 
+						. $HLFREQ[( 4 * $index + 1 )] 
+						. $HLFREQ[( 4 * $index + 0 )]
+					)
+			// )
+		);
+	}
+	
+	for($index = 1; $index < 1023; $index++){
+		array_push($TimeDomain, 
+			covint(16,
+				hexdec(
+					'0x'. $HLVIBR[2 * $index - 1] 
+					. $HLVIBR[2 * $index ]
+				)
+			)
+		);
+	}
+	
+	$peakFreq = array_keys($FreqDomain, max($FreqDomain))[0];
+	$rtnData['PEAK'] = $peakFreq;
+	$rtnData['RMS'] = number_format((float)rmsValue($TimeDomain), 1, '.', '');
+	$rtnData['AVG'] = number_format((float)avgValue($TimeDomain), 1, '.', '');
+	// echo '<pre>'.print_r($TimeDomain, true).'</pre>';
+	echo json_encode($rtnData);
 	$conn->close();
 }
 
@@ -87,14 +139,17 @@ if($_GET['r'] == 'TP0L'){
 	if ($conn->connect_error) {
 		die("Connection failed: " . $conn->connect_error);
 	}
-	$sql = 'SELECT TIMEMARK,VDATA FROM temperature WHERE STATUS = 1 ORDER BY TIMEMARK DESC LIMIT 60';
+	$sql = 'SELECT TIMEMARK,VDATA FROM temperature WHERE STATUS = 1 ORDER BY TIMEMARK DESC LIMIT 1200';
 	$result = $conn->query($sql);
-
+	$index = 0;
+	
 	$TempJson = array();
 	while($row = $result->fetch_assoc()){
-		$TempData = adc2temp(str_split($row['VDATA'],4)[0]);
-		$TimeData = explode(' ', $row['TIMEMARK'])[1];
-		$TempJson[$TimeData] = $TempData;
+		if($index++ % TP0L_MOD == 0){
+			$TempData = adc2temp(str_split($row['VDATA'],4)[0]);
+			$TimeData = explode(' ', $row['TIMEMARK'])[1];
+			$TempJson[$TimeData] = $TempData;
+		}
 	}
 	
 	//echo '<pre>'.print_r($TempData, true).'</pre>';
@@ -122,6 +177,92 @@ if($_GET['r'] == 'EDS'){
 	$conn->close();
 }
 
+if($_GET['r'] == 'EDSsingle'){
+	$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+	$sql = 'SELECT TIMEMARK,VDATA FROM EDS WHERE STATUS = 1 ORDER BY TIMEMARK DESC LIMIT 1';
+	$result = $conn->query($sql);
+
+	$EDSJson = array();
+	while($row = $result->fetch_assoc()){
+		$EDSData = adc2temp(str_split($row['VDATA'],4)[0]);
+		array_push($EDSJson, $EDSData);
+	}
+	
+	//echo '<pre>'.print_r($TempData, true).'</pre>';
+	echo json_encode(array_reverse($EDSJson));
+	$conn->close();
+}
+
+if($_GET['r'] == 'META'){
+	$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+	$sqlTD = 'SELECT TIMEMARK,SOURCEIP,SOURCEPORT FROM vibrationTD WHERE STATUS = 1 ORDER BY TIMEMARK DESC LIMIT 1';
+	$sqlTemp = 'SELECT TIMEMARK,SOURCEIP,SOURCEPORT FROM temperature WHERE STATUS = 1 ORDER BY TIMEMARK DESC LIMIT 1';
+	$sqlEDS = 'SELECT TIMEMARK,SOURCEIP,SOURCEPORT FROM EDS WHERE STATUS = 1 ORDER BY TIMEMARK DESC LIMIT 1';
+	
+	$rTD = $conn->query($sqlTD);
+	$rTemp = $conn->query($sqlTemp);
+	$rEDS = $conn->query($sqlEDS);
+
+	$metaReturn = array();
+
+	while($row = $rTD->fetch_assoc()){
+		$metaReturn['vibr'] = array(
+			'Src' => $row['SOURCEIP'].':'.$row['SOURCEPORT'],
+			'Last' => $row['TIMEMARK'],
+		);
+	}
+	while($row = $rTemp->fetch_assoc()){
+		$metaReturn['temp'] = array(
+			'Src' => $row['SOURCEIP'].':'.$row['SOURCEPORT'],
+			'Last' => $row['TIMEMARK'],
+		);
+	}
+	while($row = $rEDS->fetch_assoc()){
+		$metaReturn['eds'] = array(
+			'Src' => $row['SOURCEIP'].':'.$row['SOURCEPORT'],
+			'Last' => $row['TIMEMARK'],
+		);
+	}
+	
+	echo json_encode(array_reverse($metaReturn));
+	$conn->close();
+}
+
+if($_GET['r'] == 'CurrStat'){
+	$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+	$sqlTD = 'SELECT STATUS FROM vibrationTD ORDER BY TIMEMARK DESC LIMIT 1';
+	$sqlTemp = 'SELECT STATUS FROM temperature ORDER BY TIMEMARK DESC LIMIT 1';
+	$sqlEDS = 'SELECT STATUS FROM EDS ORDER BY TIMEMARK DESC LIMIT 1';
+	
+	$rTD = $conn->query($sqlTD);
+	$rTemp = $conn->query($sqlTemp);
+	$rEDS = $conn->query($sqlEDS);
+
+	$metaReturn = array();
+
+	while($row = $rTD->fetch_assoc()){
+		$metaReturn['vibr'] = $row['STATUS'];
+	}
+	while($row = $rTemp->fetch_assoc()){
+		$metaReturn['temp'] = $row['STATUS'];
+	}
+	while($row = $rEDS->fetch_assoc()){
+		$metaReturn['eds'] = $row['STATUS'];
+	}
+	
+	echo json_encode(array_reverse($metaReturn));
+	$conn->close();
+}
+
 function adc2temp($adc){
 	return hexdec($adc) * 0.061 - 55;
 }
@@ -135,4 +276,38 @@ function covint($bits, $input){
         return -(($input ^ ((1 << $bits)-1)) + 1);
     }
 }
+
+function rmsValue($arr) { 
+	$square = 0; 
+	$mean = 0.0;
+	$root = 0.0; 
+	
+	$n = sizeof($arr);
+	
+	// Calculate square. 
+	for ($i = 0; $i < $n; $i++){ 
+		$square += pow($arr[$i], 2); 
+	} 
+
+	// Calculate Mean. 
+	$mean = ($square / (float)($n)); 
+
+	// Calculate Root. 
+	$root = sqrt($mean); 
+
+	return $root; 
+} 
+
+function avgValue($arr) { 
+	$sum = 0.0;
+	$mean = 0.0;
+	
+	$n = sizeof($arr);
+	for ($i = 0; $i < $n; $i++){ 
+		$sum += $arr[$i]; 
+	} 
+
+	$mean = ($sum / (float)($n)); 
+	return $mean; 
+} 
 ?>
